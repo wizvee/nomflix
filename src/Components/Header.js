5da51e853ff25e50ba2220a2c5c84f81206e32b4
wizvee <jeonghye.dev@gmail.com>
2019-10-08 10:32:21 +0900

    header, router 적용

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default () => (
+  <header className="nav">
+    <ul>
+      <li>
+        <a href="/">Movies</a>
+      </li>
+      <li>
+        <a href="/tv">TV</a>
+      </li>
+      <li>
+        <a href="/search">Search</a>
+      </li>
+    </ul>
+  </header>
+);
